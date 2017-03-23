c1a3ced3cc167d91cd32ba253ad0b5cc7295b5ed
Johann150 <Johann150@users.noreply.github.com>
2017-03-23 14:57:44 +0100

    optimized the translatig system
    now the meaning of the line only has to be entered once in the /lang/dictionary file

--- translate.php
+++ translate.php
@@ -1,53 +1,65 @@
 <?php
-//your dictionary here!
-$dictionary_file="dict.ini";
-//language fallback
-$use_lang="en";
+/*
+language to use by default
+NULL or unset means to use the language provided by the browser
 
-dictionary_setup();
+can't be null if $strict is set to true
+*/
+$use_lang=null;
+/*
+wheter the translator module should be set up strict, meaning it stays with the default language
+if set on false your users might get a better user experience
+	because the page is presented in their preferred language automatically, if this language is supported
+*/
+$strict=false;
 
-if(isset($_GET['lang'])){
-	$lang=strtolower(substr($_GET['lang'],0,2));
-	if(language_supported($lang)){
-		$use_lang=$lang;
-	}else{
-		echo $_GET['lang'].' '.__('notsupported').".<br>".__('but').' ';
-	}
-}
-
-function dictionary_setup(){
-	global $dictionary_file;
-	global $dictionary;
-	global $use_lang;
-	$dictionary_read=file($dictionary_file);
-	$dictionary=array();
-	$found_lang=false;
-	foreach($dictionary_read as $l){
-		if(strpos($l,'[')!==FALSE){//new language
-			if($found_lang){//push previous language
-				$dictionary[$found_lang]=$lang;
-			}
-			$lang=array();//reset current language
-			$found_lang=substr($l,1,strlen($l)-4);//put name of active language
-			continue;
-		}elseif(strpos($l,':')===FALSE){continue;}//skip invalid lines
-		$l=trim(explode("#",$l, 2)[0]);//remove comments (starting with #)
-		if($l==''){continue;}//skip empty lines
-		if(!$found_lang){continue;}//skip if a language was not yet found
-		list($key,$value)=explode(":",$l,2);//explode key:value pairs
-		$lang[$key]=$value;
-	}
-	$dictionary[$found_lang]=$lang;
-
-	$langs=explode(',',$_SERVER['HTTP_ACCEPT_LANGUAGE']);
-	for($i=0;$i<count($langs);$i++){
-		if(language_supported(substr($langs[$i],0,2))){
-			$use_lang=substr($langs[$i],0,2);
-			break;
+//a get parameter called lang overrides the default language, if not in strict mode
+if(!$strict){
+	if(isset($_GET['lang'])){
+		$lang=strtolower(substr($_GET['lang'],0,2));
+		if(language_supported($lang)){
+			$use_lang=$lang;
+		}else{
+			echo $_GET['lang'].' '.__('notsupported').".<br>".__('but').' ';
 		}
 	}
 }
 
+dictionary_setup();
+
+function dictionary_setup($strict=false){
+	global $dictionary;
+	global $use_lang;
+	$dictionary=array();
+	$template=file("lang/dictionary");
+	$dir=scandir("lang/");
+	foreach($dir as $f){
+		if($f=='.'||$f=='..'||$f=='dictionary') continue;
+		$dictionary_file=file("lang/$f");
+		for($i=0;$i<count($dictionary_file);$i++){
+			$key=$template[$i];
+			$dictionary[$f][$key]=$dictionary_file[$i];
+		}
+	}
+	if(!$strict&&isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])){
+		//selects the users preferred language (from browser settings)
+		$langs=explode(',',$_SERVER['HTTP_ACCEPT_LANGUAGE']);
+		foreach($langs as $l){
+			if(language_supported($l)){
+				$use_lang=$l;
+				break;
+			}
+		}
+	}
+}
+
+function language_supported($lang){
+	return file_exists("lang/".$lang);
+}
+
+/**
+the actual translating function
+*/
 function __($translate,$lang=null){
 	global $use_lang;
 	global $dictionary;
@@ -56,10 +68,3 @@ function __($translate,$lang=null){
 	}
 	return $dictionary[$lang][$translate];
 }
-
-function language_supported($lang){
-	global $dictionary;
-	return in_array($lang,array_keys($dictionary));
-}
-
-?>
