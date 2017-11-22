9e0a1b638ac0adfbe0f6ab774011de2187d99575
Johann150 <johann.galle@protonmail.com>
2017-11-22 14:12:50 +0100

    now working
    without jquery ;-)

--- back.php
+++ back.php
@@ -1,13 +1,6 @@
 <?php
 include "translate.php";
-if (isset($_GET['langs'])) {
-    header_remove();
-    http_response_code(200);
-    header("Content-type:application/json");
-    echo json_encode(get_languages());
-}elseif (isset($_GET['keys'])){
-    header_remove();
-    http_response_code(200);
-    header("Content-tyüe: application/json");
-    echo json_encode(array_keys(get_languages()));
-}
+header_remove();
+http_response_code(200);
+header("Content-type:application/json");
+echo json_encode($dictionary);
--- resources/translator.js
+++ resources/translator.js
@@ -1,1 +1,31 @@
-
+var req = new XMLHttpRequest();
+req.open("GET","back.php");
+req.addEventListener("load",()=>{
+	if(req.status>=200 && req.status < 300){
+		var data=JSON.parse(req.responseText);
+		for(var t in data){
+			var h = document.createElement("th");
+			h.innerHTML=data[t].language;
+			document.getElementById("tb-head").append(h);
+		}
+		var langs=[];
+		for(var l in data){
+			langs.push(l);
+		}
+		for(var key in data[langs[1]]){
+			var h = document.createElement("tr");
+			var h1 = document.createElement("td");
+			h1.setAttribute("contenteditable","true");
+			h1.innerHTML = key;
+			h.appendChild(h1);
+			for(var lang of langs){
+				h1 = document.createElement("td");
+				h1.innerHTML = data[lang][key];
+				h1.setAttribute("contenteditable","true");
+				h.appendChild(h1);
+			}
+			document.getElementById("tbody").appendChild(h)
+		}
+	}
+});
+req.send();
