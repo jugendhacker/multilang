50fe5db6ca1557fc0f8d87056f8bbbbe6ee2552c
Johann150 <johann.galle@protonmail.com>
2017-11-22 22:57:31 +0100

    reconstructing dictionary
    now I only got to save it to disk

--- back.php
+++ back.php
@@ -1,20 +1,33 @@
 <?php
 include "translate.php";
-if (isset($_GET['langs'])) {
-    header_remove();
-    http_response_code(200);
-    header("Content-type:application/json");
-    echo json_encode($dictionary);
+if (isset($_GET['langs'])){
+	header_remove();
+	http_response_code(200);
+	header("Content-type:application/json");
+	echo json_encode($dictionary);
 }elseif (isset($_GET['keys'])){
-    header_remove();
-    http_response_code(200);
-    header("Content-type: application/json");
-    echo json_encode(array_keys($dictionary[array_keys($dictionary)[0]]));
+	header_remove();
+	http_response_code(200);
+	header("Content-type: application/json");
+	echo json_encode(array_keys($dictionary[array_keys($dictionary)[0]]));
 }elseif (isset($_POST)){
-    header_remove();
-    http_response_code(200);
-    save($HTTP_RAW_POST_DATA);
+	header_remove();
+	http_response_code(200);
+	save($HTTP_RAW_POST_DATA);
 }
 function save($data){
-    echo var_dump(json_decode($data));
+	global $dictionary;
+	$data=json_decode($data);
+	$dictionary=array();
+	for ($i=0; $i < count($data); $i++) {
+		$alias='';
+		foreach ($data[$i] as $key => $value) {
+			if($key=='Key'){
+				$alias=$value;
+			}else{
+				$dictionary[$key][$alias]=$value;
+			}
+		}
+	}
+	var_dump($dictionary);
 }
