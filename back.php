07e2fcaade058a5f04c078f6c4cbcc9856e30eff
jer <julian.ribbeck@gmx.de>
2017-11-22 19:43:12 +0100

    with post request, but no file saving

--- back.php
+++ back.php
@@ -10,4 +10,11 @@ if (isset($_GET['langs'])) {
     http_response_code(200);
     header("Content-type: application/json");
     echo json_encode(array_keys($dictionary[array_keys($dictionary)[0]]));
+}elseif (isset($_POST)){
+    header_remove();
+    http_response_code(200);
+    save($HTTP_RAW_POST_DATA);
+}
+function save($data){
+    echo var_dump(json_decode($data));
 }
